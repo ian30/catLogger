10e4c8ef65289622df9433f6090498f7cf408c00
Ian Shnayder <ian.shnayder@hyland.com>
2023-06-21 18:31:06 -0700

    Changed button text to be more clear (save new cat), added cancel button that disables the "add cat" button and bring it back once new cat input is cancelled.

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 function removeCat(catIndex) {
-  cats.splice(catIndex, 1); // Remove the cat from the array
-  renderCatList(); // Re-render the cat list 123
+    cats.splice(catIndex, 1); // Remove the cat from the array
+    renderCatList(); // Re-render the cat list
 }
 
 function toggleCatVisibility(catIndex) {
-  cats[catIndex].hidden = !cats[catIndex].hidden; // Toggle the hidden property
-  renderCatList(); // Re-render the cat list
+    cats[catIndex].hidden = !cats[catIndex].hidden; // Toggle the hidden property
+    renderCatList(); // Re-render the cat list
 }
 
 function openAddCatForm() {
-  const catForm = document.createElement("form");
-  catForm.className = "add-cat-form";
-  catForm.innerHTML = `
-    <label for="name">Name:</label>
+    const addCatButton = document.querySelector('.add-cat-btn button');
+    addCatButton.disabled = true; // Disable the "Add a cat" button
+    const catForm = document.createElement("form");
+    catForm.className = "add-cat-form";
+    catForm.innerHTML = `
+    <label for="name" class="dsp-block">Name:</label>
     <input type="text" id="name" name="name" required><br>
 
-    <label for="dob">Date of Birth:</label>
+    <label for="dob" class="dsp-block">Date of Birth:</label>
     <input type="date" id="dob" name="dob" required><br>
 
-    <label for="sex">Sex:</label>
+    <label for="sex" class="dsp-block">Sex:</label>
     <select id="sex" name="sex" required>
       <option value="Male">Male</option>
       <option value="Female">Female</option>
     </select><br>
-    <label for="breed">Breed:</label>
+    <label for="breed" class="dsp-block">Breed:</label>
     <select id="breed" name="breed" class="form-select" required>
-      <option value="Abyssinian">Abyssinian</option>
+      <option value="American Short Hair">American Short Hair</option>
       <option value="British Shorthair">British Shorthair</option>
+      <option value="Other">Other / Don't Know</option>
       <option value="Burmese">Burmese</option>
       <option value="Cornish Rex">Cornish Rex</option>
       <option value="Devon Rex">Devon Rex</option>
@@ -42,25 +45,32 @@ function openAddCatForm() {
       <option value="Turkish Van">Turkish Van</option>
     </select><br>
 
-    <label for="fixed">Fixed:</label>
-    <input type="checkbox" id="fixed" name="fixed"><br>
+    <fieldset>
+      <label for="fixed">Fixed:</label><input type="checkbox" id="fixed" name="fixed"><br>
+      <label for="shots">Shots:</label><input type="checkbox" id="shots" name="shots"><br>
+    </fieldset>
 
-    <label for="shots">Shots:</label>
-    <input type="checkbox" id="shots" name="shots"><br>
-
-    <label for="weight">Weight:</label>
+    <label for="weight" class="dsp-block">Weight:</label>
     <input type="number" id="weight" name="weight" step="0.1" required><br>
 
-    <label for="special_notes">Special Notes:</label>
+    <label for="special_notes" class="dsp-block">Special Notes:</label>
     <textarea id="special_notes" name="special_notes"></textarea><br>
 
-    <button type="submit">Add</button>
+    <button type="submit">Save New Cat</button>
+    <button type="button" onclick="cancelAddCat()">Cancel</button>
   `;
 
-  catForm.addEventListener("submit", addCat);
+    catForm.addEventListener("submit", addCat);
 
-  const catList = document.getElementById("catList");
-  catList.appendChild(catForm);
+    const catList = document.getElementById("catList");
+    catList.appendChild(catForm);
+}
+function cancelAddCat() {
+    const addCatButton = document.querySelector('.add-cat-btn button');
+    addCatButton.disabled = false; // Enable the "Add a cat" button
+
+    const catForm = document.querySelector('.add-cat-form');
+    catForm.remove();
 }
 
 function addCat(event) {
@@ -109,15 +119,15 @@ function addCat(event) {
 let cats = [];
 
 function renderCatList() {
-  const catList = document.getElementById("catList");
-  catList.innerHTML = "";
+    const catList = document.getElementById("catList");
+    catList.innerHTML = "";
 
-  cats.forEach((cat) => {
-    const listItem = document.createElement("li");
-    const age = new Date().getFullYear() - new Date(cat.dob).getFullYear();
-    listItem.className = "cat-entry";
+    cats.forEach((cat) => {
+        const listItem = document.createElement("li");
+        const age = new Date().getFullYear() - new Date(cat.dob).getFullYear();
+        listItem.className = "cat-entry";
 
-    listItem.innerHTML = `
+        listItem.innerHTML = `
       <strong>Name:</strong> ${cat.name}<br>
       <strong>Date of Birth:</strong> ${cat.dob}<br>
       <strong>Age:</strong> ${age} years<br>
@@ -128,15 +138,15 @@ function renderCatList() {
       <strong>Special Notes:</strong> ${cat.special_notes}
     `;
 
-    catList.appendChild(listItem);
-  });
+        catList.appendChild(listItem);
+    });
 }
 
 // Fetch cats from JSON file
 fetch("cats.json")
-  .then((response) => response.json())
-  .then((data) => {
-    cats = data;
-    renderCatList();
-  })
-  .catch((error) => console.error("Error:", error));
+    .then((response) => response.json())
+    .then((data) => {
+        cats = data;
+        renderCatList();
+    })
+    .catch((error) => console.error("Error:", error));
--- inc/express.js
+++ inc/express.js
@@ -9,7 +9,7 @@ app.post('/cats', (req, res) => {
     const catData = req.body;
 
     // Append the cat data to the text file
-    fs.appendFile('catData.txt', JSON.stringify(catData) + '\n', (err) => {
+    fs.appendFile('../catData.txt', JSON.stringify(catData) + '\n', (err) => {
         if (err) {
             console.error('Error saving cat data:', err);
             res.status(500).json({ error: 'Error saving cat data' });
